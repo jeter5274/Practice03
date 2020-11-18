096d43ee21db4caa4d0c27caec784bb17945d2e4
jeter5274 <jeter5274@gmail.com>
2020-11-18 15:47:43 +0900

    equals메소드 설명 추가

--- src/com/javaex/practice/Ex20.java
+++ src/com/javaex/practice/Ex20.java
@@ -25,9 +25,9 @@ public class Ex20 {
 				
 				System.out.println("맞았습니다.");
 				System.out.print("게임을 종료하시겠습니까?(y/n) >>");
-				char endgame = sc.next().charAt(0);
+				char endgame = sc.next().charAt(0); //charAt(0)는 스트링의 0번째 문자라는 의미
 				
-				if(endgame == 'y' ) {
+				if(endgame == 'y' ) { //endgame.equal("y");로 스트링 비교 가능
 					System.out.println("==============================");
 					System.out.println("    [숫자맞추기게임 종료]");
 					System.out.println("==============================");
